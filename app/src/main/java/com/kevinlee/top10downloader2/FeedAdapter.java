1075fa784c4d40208aa8b6793a5f0b8ffac413c4
KevinLeeProjects <kevinleela@hotmail.com>
2019-03-31 04:37:48 -0700

    Changed whole XML Reader so it works on physical devices

---
+++ app/src/main/java/com/kevinlee/top10downloader2/FeedAdapter.java
@@ -0,0 +1,142 @@
+package com.kevinlee.top10downloader2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.provider.ContactsContract;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+public class FeedAdapter extends ArrayAdapter {
+    private static final String TAG = "FeedAdapter";
+    private final int layoutResource;
+    private final LayoutInflater layoutInflater;
+    private List<FeedEntry> applications;
+
+    public FeedAdapter(Context context, int resource, List<FeedEntry> applications) {
+        super(context, resource);
+        this.layoutResource = resource;
+        this.layoutInflater = LayoutInflater.from(context);
+        this.applications = applications;
+    }
+
+    @Override
+    public int getCount() {
+        return applications.size();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView==null) {
+            Log.d(TAG, "getView: called with null convertView");
+            convertView = layoutInflater.inflate(layoutResource, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            Log.d(TAG, "getView: provided a convertView");
+
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+//        TextView tvName = (TextView) convertView.findViewById(R.id.tvName);
+//        TextView tvArtist = (TextView) convertView.findViewById(R.id.tvArtist);
+//        TextView tvSummary = (TextView) convertView.findViewById(R.id.tvSummary);
+
+        final FeedEntry currentApp = applications.get(position);
+
+        viewHolder.tvName.setText(currentApp.getName());
+        viewHolder.tvArtist.setText(currentApp.getArtist());
+        viewHolder.tvSummary.setText(currentApp.getSummary());
+        //View v = LayoutInflater.from(getContext()).inflate(R.layout.list_record, null);
+        ImageView iv = (ImageView)convertView.findViewById(R.id.imageView);
+        DownloadImage download = new DownloadImage(iv);
+        download.execute(currentApp.getImageURL());
+//        ImageView image = (ImageView) convertView.findViewById(R.id.imageView);
+//       try {
+//            image.setBackground(drawableFromUrl(currentApp.getImageURL()));
+//        } catch (Exception e){
+//           e.printStackTrace();
+//        }
+
+//        Picasso.get().load(currentApp.getImageURL()).into(viewHolder.image);
+        iv.setOnClickListener( new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(currentApp.getLink()));
+                v.getContext().startActivity(intent);
+            }
+});
+        return convertView;
+    }
+
+    public class DownloadImage extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImage(ImageView bmImage) {
+            this.bmImage = (ImageView ) bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.d("Error", e.getStackTrace().toString());
+
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+        }
+    }
+    public Drawable drawableFromUrl(String url) throws IOException {
+        Bitmap x;
+
+        HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+        connection.connect();
+        InputStream input = connection.getInputStream();
+
+        x = BitmapFactory.decodeStream(input);
+        return new BitmapDrawable(x);
+    }
+
+    private class ViewHolder{
+        final TextView tvName;
+        final TextView tvArtist;
+        final TextView tvSummary;
+
+        ViewHolder(View v){
+            this.tvName = v.findViewById(R.id.tvName);
+            this.tvArtist = v.findViewById(R.id.tvArtist);
+            this.tvSummary = v.findViewById(R.id.tvSummary);
+        }
+    }
+}
---
+++ app/src/main/java/com/kevinlee/top10downloader2/FeedEntry.java
@@ -0,0 +1,69 @@
+package com.kevinlee.top10downloader2;
+
+public class FeedEntry {
+
+    private String name;
+    private String artist;
+    private String releaseDate;
+    private String summary;
+    private String imageURL;
+    private String link;
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+    @Override
+    public String toString() {
+        return "FeedEntry{" +
+                "name=" + name + '\n' +
+                ", artist=" + artist + '\n' +
+                ", releaseDate=" + releaseDate + '\n' +
+                ", imageURL=" + imageURL + '\n' +
+                ", Link=" + link + '\n';
+    }
+}
